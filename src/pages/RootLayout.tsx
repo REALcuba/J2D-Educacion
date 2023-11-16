7c85d07711951641bca6376ff5fedb04d92e7121
Javi Extremera <javi.extremera92@gmail.com>
2023-11-16 15:34:27 +0100

    commit

---
+++ src/pages/RootLayout.tsx
@@ -0,0 +1,10 @@
+export const RootLayout = () => {
+  return (
+    <main className="min-h-screen outline outline-red-400">
+        <aside className="outline outline-cyan-600 w-1/4 md:w- min-h-screen">
+            <h1>Aside</h1>
+        </aside>
+
+    </main>
+  )
+}
